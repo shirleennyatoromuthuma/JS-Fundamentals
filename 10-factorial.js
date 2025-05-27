49426ac205c10e064bef453f4eafee323fcd2cbc
shirleennyatoromuthuma <shirleenmuthush123@gmail.com>
2025-05-27 23:50:07 +0300

    Add recursive factorial function script

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+     if (isNaN(n)) {
+            return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
